b6e86291e28793a03bcdfad15905bf2b97153436
Brady <bdullanty@ewu.edu>
2024-04-13 12:41:30 -0700

    Implemented getFileName(), openFile(), getGuessColumn(), added in test.csv data

--- Program/UserInput.py
+++ Program/UserInput.py
@@ -1,10 +1,22 @@
+
+import pandas as pd
 def getFileName():
-    return None
-
-
+    fileName = input("Please enter a file name: ")
+    while (True):
+        try:
+            pd.read_csv(fileName)
+            return fileName
+        except:
+            print("Please enter a valid file name in the same directory as this .py file")
+            fileName = input()
 def openFile(fileName):
-    return None
+    return pd.read_csv(fileName)
 
 
-def getGuessColumn():
-    return None
+def getGuessColumn(file,fileName):
+    while(True):
+        chosenCol = input("Please enter a valid column from file "+fileName)
+        for colName in file.columns:
+            if(colName == chosenCol):
+                return colName
+
--- Program/main.py
+++ Program/main.py
@@ -8,13 +8,12 @@ def main():
     print("Welcome to our data set predictor!")
     fileName = UserInput.getFileName()
     originalFile = UserInput.openFile(fileName)
+    print(fileName)
+    guessColumn = UserInput.getGuessColumn(originalFile,fileName)
+
     processedFile = PreProcess.processFile(originalFile)
-    guessColumn = UserInput.getGuessColumn()
     results = Classification.makeGuesses(processedFile,guessColumn)
     Classification.printAccuracy(processedFile,results)
-    #
-    #
-    #
 
 if __name__ == "__main__":
     main()
