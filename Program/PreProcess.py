875deef5859b35fa92761333cdc12893cd27868c
mlegard <159670685+mlegard@users.noreply.github.com>
2024-04-13 17:30:45 -0700

    Update PreProcess.py

--- Program/PreProcess.py
+++ Program/PreProcess.py
@@ -4,13 +4,13 @@ from sklearn.impute import SimpleImputer
 
 
 def processFile(data):
-    # Handle missing values (example: numeric imputation)
+    
     for col in data.select_dtypes(include=['float64', 'int64']):
         data[col].fillna(data[col].mean(), inplace=True)
 
     # Encode categorical data
     for col in data.select_dtypes(include=['object']):
-        if data[col].nunique() <= 10:  # arbitrary cutoff for categorical vs nominal
+        if data[col].nunique() <= 1000:  # arbitrary cutoff for categorical vs nominal
             le = LabelEncoder()
             data[col] = le.fit_transform(data[col])
         else:
